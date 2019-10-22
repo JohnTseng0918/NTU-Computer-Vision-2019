f1e3394bafcd8d4d295e5611cd6907795822ddef
John Tseng <s9141302@gmail.com>
2019-10-22 11:22:30 +0800

    add dilation

---
+++ hw4/cvhwa.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+
+im=Image.open("lena.bmp")
+row,col=im.size
+
+img=np.empty((row,col),dtype=int)
+kernel=np.array([[0,1,1,1,0],
+                 [1,1,1,1,1],
+                 [1,1,1,1,1],
+                 [1,1,1,1,1],
+                 [0,1,1,1,0]])
+dilation=np.zeros((row,col),dtype=int)
+
+
+for i in range(row):
+    for j in range(col):
+        tmp=im.getpixel((i,j))
+        if tmp>=128:
+            img[i][j]=1
+        else:
+            img[i][j]=0
+
+for i in range(row):
+    for j in range(col):
+        if(img[i][j]==1):
+            for x in range(-2,3,1):
+                if x+i>=512:
+                    break
+                for y in range(-2,3,1):
+                    if j+y>=512:
+                        break
+                    dilation[i+x][j+y]=255
+
+dilation=dilation.transpose()
+im=Image.fromarray(dilation)
+im=im.convert("L")
+im.show()
+im.save('lena_dilation.bmp',format='BMP')
